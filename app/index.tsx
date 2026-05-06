6ddd19f0a7fdff8950f2e40e419465428c8e3949
Sashirusliit <it22626356@my.sliit.lk>
2026-05-06 22:28:10 +0530

    inital commit

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,13 @@ import { router } from "expo-router";
 import { useEffect } from "react";
 import { ActivityIndicator, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { tr } from "zod/v4/locales";
 
 export default function Index() {
-  const { isSignedIn, isLoaded } = useAuth();
+  // const { isSignedIn, isLoaded } = useAuth();
+
+  let isSignedIn = true;
+  let isLoaded = true;
 
   useEffect(() => {
     if (!isLoaded) return;
